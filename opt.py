4c758106e7bfe7fae5185a4700b7b3b52b173422
Swathi. A <32516016+swathi-a@users.noreply.github.com>
2024-10-07 18:59:08 +0900

    Add files via upload

---
+++ opt.py
@@ -0,0 +1,6 @@
+import torch 
+from botoch.testfunction import Branin
+from botorch.optim import optimize_acqf
+from botorch.models import SingeTaskGP
+from botorch.fit import fit_gpytorch_model
+from 
